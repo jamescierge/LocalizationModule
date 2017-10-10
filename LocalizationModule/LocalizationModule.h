7a821efd291f63070968383614ed5ae584d878a5
James McPherson <james.mcpherson@monscierge.com>
2017-10-10 15:04:25 -0500

    Created LocalizationModule Project

---
+++ LocalizationModule/LocalizationModule.h
@@ -0,0 +1,19 @@
+//
+//  LocalizationModule.h
+//  LocalizationModule
+//
+//  Created by James McPherson on 10/10/17.
+//  Copyright © 2017 James McPherson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalizationModule.
+FOUNDATION_EXPORT double LocalizationModuleVersionNumber;
+
+//! Project version string for LocalizationModule.
+FOUNDATION_EXPORT const unsigned char LocalizationModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizationModule/PublicHeader.h>
+
+
